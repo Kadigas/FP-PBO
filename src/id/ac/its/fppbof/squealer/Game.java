00e91f1f068443637a073de51bad1704fb2e2d32
SteelAwsm <hr.heriqbaldi@outlook.com>
2021-12-01 22:53:09 +0700

    i made it sus
    sussy ඞ ඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞ

--- src/id/ac/its/fppbof/squealer/Game.java
+++ src/id/ac/its/fppbof/squealer/Game.java
@@ -3,8 +3,7 @@ package id.ac.its.fppbof.squealer;
 public class Game {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
-
+		System.out.println("Hello world is sus");
 	}
 
 }
