238158d01e9661a30ff6bedf67e761aa195f8f0d
Richo <daininoricho@yahoo.com>
2021-12-01 23:00:08 +0800

    Testing File Project

--- src/id/ac/its/fppbof/squealer/Game.java
+++ src/id/ac/its/fppbof/squealer/Game.java
@@ -3,7 +3,7 @@ package id.ac.its.fppbof.squealer;
 public class Game {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello world");
 
 	}
 
