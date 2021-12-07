3ce76ed4e4ccd5897039afcd3555ae6c1f6f70eb
Richo <daininoricho@yahoo.com>
2021-12-07 13:01:55 +0700

    Adding GameStateManager and Tilemap
    This code is created by Hemakesha Ramadhani but he can't commit this to the repo
    
    Co-Authored-By: SteelAwsm <78362238+SteelAwsm@users.noreply.github.com>

---
+++ src/gamestate/Level1State.java
@@ -0,0 +1,42 @@
+package gamestate;
+
+import id.ac.its.fppbof.squealer.GamePanel;
+import tilemap.*;
+import java.awt.*;
+
+public class Level1State extends GameState {
+		
+	private TileMap tileMap;
+	
+	public Level1State(GameStateManager gsm) {
+		this.gsm = gsm;
+		init();
+	}
+	
+	public void init() {
+		
+		tileMap = new TileMap(30);
+		tileMap.loadTiles("/Tilesets/grasstileset.gif");
+		tileMap.loadMap("/Maps/level1-1.map");
+		tileMap.setPosition(0, 0);
+		
+	}
+	
+	
+	public void update() {}
+	
+	public void draw(Graphics2D g) {
+		
+		// clear screen
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, GamePanel.WIDTH, GamePanel.HEIGHT);
+		
+		// draw tilemap
+		tileMap.draw(g);
+		
+	}
+	
+	public void keyPressed(int k) {}
+	
+	public void keyReleased(int k) {}
+}
--- src/id/ac/its/fppbof/squealer/Game.java
+++ src/id/ac/its/fppbof/squealer/Game.java
@@ -1,9 +1,19 @@
 package id.ac.its.fppbof.squealer;
 
+import javax.swing.JFrame;
+
 public class Game {
 
-	public static void main(String[] args) {
-		System.out.println("Hello world is sus");
+	public static void main(String[] args) 
+	{
+		
+		JFrame window = new JFrame("Squealer");
+		window.setContentPane(new GamePanel());
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(false);
+		window.pack();
+		window.setVisible(true);
+		
 	}
 
 }
